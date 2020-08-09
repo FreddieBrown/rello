27adf39d2c6acc2fdba9569e8e6fbab0284ff3b2
Freddie Brown <fred@noser.net>
2020-08-09 12:35:22 +0100

    added cli options
    completed all CLI options and now app works fully

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+#![allow(dead_code)]
 use std::{fs::File, io::prelude::*, fmt};
+use std::io::{stdin,stdout,Write};
 use clap::{Arg, App};
 use serde::Deserialize;
 use serde::Serialize;
@@ -116,6 +118,20 @@ impl Board {
         None
     }
 
+    fn read_text(question: String) -> String {
+        let mut s=String::new();
+        print!("{}: ", question);
+        let _=stdout().flush();
+        stdin().read_line(&mut s).expect("Did not enter a correct string");
+        if let Some('\n')=s.chars().next_back() {
+            s.pop();
+        }
+        if let Some('\r')=s.chars().next_back() {
+            s.pop();
+        }
+        s
+    }
+
 
 }
 
@@ -183,22 +199,21 @@ fn main() {
             .takes_value(true))
         .arg(Arg::with_name("edit_item")
             .long("edit_item")
-            .help("Edit Item Data"))
+            .help("Edit Item Data")
+            .takes_value(true))
         .arg(Arg::with_name("add_item")
             .long("add_item")
-            .help("Add Item to Column")
-            .takes_value(true))
+            .help("Add Item to Column"))
         .arg(Arg::with_name("remove_item")
             .long("remove_item")
             .help("Remove Item From Column")
             .takes_value(true))
         .arg(Arg::with_name("move_item")
             .long("move_item")
-            .help("Move Item To Different Column")
-            .takes_value(true))
+            .help("Move Item To Different Column"))
         .get_matches();
     let mut config_toml = String::from("");
-    let file_path = "board.toml";
+    let file_path = "./board.toml";
     let mut file = match File::open(&file_path) {
         Ok(file) => file,
         Err(_) => {
@@ -208,15 +223,15 @@ fn main() {
     file.read_to_string(&mut config_toml)
         .unwrap_or_else(|err| panic!("Error while reading config: [{}]", err));
 
-    let mut config: Board = toml::from_str(&config_toml).unwrap();
+    let mut board: Board = toml::from_str(&config_toml).unwrap();
     
     if matches.is_present("list"){
-        println!("{}", config.to_string());
+        println!("{}", board.to_string());
     }
 
     if matches.is_present("add_column"){
         match matches.value_of("add_column"){
-            Some(name) => config.create_column(String::from(name)),
+            Some(name) => board.create_column(String::from(name)),
             None => ()
         };
 
@@ -225,7 +240,7 @@ fn main() {
 
     if matches.is_present("remove_column"){
         match matches.value_of("remove_column"){
-            Some(name) => config.remove_column(String::from(name)),
+            Some(name) => board.remove_column(String::from(name)),
             None => ()
         };
 
@@ -233,12 +248,18 @@ fn main() {
     }
 
     if matches.is_present("add_item"){
-        println!("Adding Item")
+
+        let title = Board::read_text(String::from("Enter Title"));
+        let body = Board::read_text(String::from("Enter Body"));
+        let col_title = Board::read_text(String::from("Enter Column"));
+
+        board.create_item(title, body, col_title);
+        println!("Item Added!");
     }
 
     if matches.is_present("remove_item"){
         match matches.value_of("remove_item"){
-            Some(id) => config.remove_item(id.parse::<u16>().unwrap()),
+            Some(id) => board.remove_item(id.parse::<u16>().unwrap()),
             None => ()
         };
 
@@ -246,13 +267,32 @@ fn main() {
     }
 
     if matches.is_present("move_item"){
-        println!("Moving Item")
+        let id = Board::read_text(String::from("Enter Item Id")).parse::<u16>().unwrap();
+        let col_title = Board::read_text(String::from("Enter Column"));
+        board.move_item(id, col_title);
+        println!("Moved Item")
     }
 
     if matches.is_present("edit_item"){
-        println!("Editing Item")
+        let blank = String::from("");
+        let title = Board::read_text(String::from("New Title (Press Enter If No Change)"));
+        if title != blank{
+            board.edit_item_title(matches.value_of("edit_item").unwrap().parse::<u16>().unwrap(), title);
+        }
+        else {
+            println!("Blank Title")
+        }
+        let body = Board::read_text(String::from("New Body (Press Enter If No Change)"));
+        if body != blank{
+            board.edit_item_body(matches.value_of("edit_item").unwrap().parse::<u16>().unwrap(), body);
+        }
+        else {
+            println!("Blank Body")
+        }
+
+        println!("Item Edited!");
     }
-    close(&config, file_path);
+    close(&board, file_path);
 }
 
 fn close(board: &Board, file: &str) {
